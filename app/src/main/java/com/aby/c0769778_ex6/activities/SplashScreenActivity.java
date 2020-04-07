b54d7e05ad7221c9bc698828a3023449a61ec840
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-07 08:06:20 -0400

    Added splash screen

---
+++ app/src/main/java/com/aby/c0769778_ex6/activities/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.aby.c0769778_ex6.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.aby.c0769778_ex6.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,AttractionListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+    }
+}
