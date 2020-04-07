1293d9fcd6178dfecdda12709dee7697a393fffd
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-07 09:19:51 -0400

    Made ui changes to details activity

--- app/src/main/java/com/aby/c0769778_ex6/activities/AttractionDetailsActivity.java
+++ app/src/main/java/com/aby/c0769778_ex6/activities/AttractionDetailsActivity.java
@@ -2,11 +2,13 @@ package com.aby.c0769778_ex6.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.aby.c0769778_ex6.R;
-import com.aby.c0769778_ex6.adapters.AttractionsAdapter;
 import com.aby.c0769778_ex6.model.CanadaAttractions;
 
 public class AttractionDetailsActivity extends AppCompatActivity {
@@ -17,22 +19,38 @@ public class AttractionDetailsActivity extends AppCompatActivity {
     private TextView txtAddress;
     private TextView txtProvince;
     private TextView txtDescription;
+    private ImageView imgBtnBack;
+    private ImageView imgAttractions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attraction_details);
 
+        getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+
+        imgBtnBack = findViewById(R.id.imgBtnBack);
         txtName = findViewById(R.id.txtName);
         txtCity = findViewById(R.id.txtCity);
         txtAddress = findViewById(R.id.txtAddress);
         txtProvince = findViewById(R.id.txtProvince);
         txtDescription = findViewById(R.id.txtDescription);
+        imgAttractions = findViewById(R.id.imgAttraction);
 
+        imgBtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         if (getIntent().getExtras() != null) {
             att = (CanadaAttractions) getIntent().getExtras().getSerializable("attractionsKey");
             txtName.setText(att.getName());
-
+            txtCity.setText(att.getCity());
+            txtAddress.setText(att.getAddress());
+            txtProvince.setText(att.getProvince());
+            txtDescription.setText(att.getDescription());
+            imgAttractions.setImageResource(att.getAttractionImg());
         }
     }
 }
